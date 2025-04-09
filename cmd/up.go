c86d9a934589524d9c6e749e5fb6c0161139ff45
mellgit <daniilhorn8@gmail.com>
2025-04-09 22:21:04 +0300

    feat: init config and logger

--- cmd/up.go
+++ cmd/up.go
@@ -1,12 +1,41 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
+	"github.com/mellgit/someuser/internal/config"
+	"github.com/mellgit/someuser/pkg/logger"
+
+	log "github.com/sirupsen/logrus"
 	"time"
 )
 
 func Up() {
-	fmt.Println("Up")
+
+	cfgPath := flag.String("config", "config.yml", "config file path")
+	flag.Parse()
+
+	cfg, err := config.LoadConfig(*cfgPath)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"action": "config.LoadConfig",
+		}).Fatal(err)
+	}
+	envCfg, err := config.LoadEnvConfig()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"action": "config.LoadEnvConfig",
+		}).Fatal(err)
+	}
+
+	if err = logger.SetUpLogger(*cfg); err != nil {
+		log.WithFields(log.Fields{
+			"action": "logger.SetUpLogger",
+		}).Fatal(err)
+	}
+
+	log.Debugf("config: %+v", cfg)
+	log.Debugf("env: %+v", envCfg)
 
 	cnt := 0
 	for {
