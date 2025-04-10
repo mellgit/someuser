0291c64d7c938fe1759778df7eea9678711922d3
mellgit <daniilhorn8@gmail.com>
2025-04-10 11:49:20 +0300

    refactor: model request

--- internal/model/someuser.go
+++ internal/model/someuser.go
@@ -4,7 +4,6 @@ type CreateUserRequest struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	Database string `json:"database"`
 }
 
 type UpdateUserRequest struct {
