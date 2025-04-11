6ddcba54cf5c0df522a122d37a1bc3c8b40f9309
mellgit <daniilhorn8@gmail.com>
2025-04-11 18:41:47 +0300

    feat: create user query in mongodb

--- internal/repository/mongo/mongo.go
+++ internal/repository/mongo/mongo.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mellgit/someuser/internal/config"
 	"github.com/mellgit/someuser/internal/model"
 	"github.com/mellgit/someuser/internal/repository"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
@@ -19,13 +20,18 @@ type MongoRepository struct {
 
 func NewMongoRepository(envCfg config.EnvConfig) (repository.Repository, error) {
 
-	uri := fmt.Sprintf("mongodb://%s:%v", envCfg.MongoHost, envCfg.MongoPort)
-	client, _ := mongo.Connect(options.Client().ApplyURI(uri))
+	uri := fmt.Sprintf("mongodb://%s:%s@%s:%d", envCfg.MongoUser, envCfg.MongoPassword, envCfg.MongoHost, envCfg.MongoPort)
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("mongo connect err: %v", err)
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	_ = client.Ping(ctx, readpref.Primary())
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("mongo ping err: %v", err)
+	}
 
 	collection := client.Database(envCfg.MongoDB).Collection(envCfg.MongoCollection)
 
@@ -36,8 +42,22 @@ func NewMongoRepository(envCfg config.EnvConfig) (repository.Repository, error) 
 }
 
 func (m MongoRepository) CreateUser(ctx context.Context, request model.CreateUserRequest) (*model.SchemaSomeUser, error) {
-	//TODO implement me
-	panic("implement me")
+
+	res, err := m.collection.InsertOne(ctx, request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert user: %w", err)
+	}
+
+	resID := res.InsertedID
+	var user model.SchemaSomeUser
+	filter := bson.M{"_id": resID}
+	err = m.collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find inserted document: %w", err)
+	}
+	user.ID = resID.(bson.ObjectID).Hex()
+
+	return &user, nil
 }
 
 func (m MongoRepository) GetAllUsers(ctx context.Context) (*[]model.SchemaSomeUser, error) {
