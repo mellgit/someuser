909c40ac07711a9bff0cc33829d21ec0c1d9be12
mellgit <daniilhorn8@gmail.com>
2025-04-14 18:55:22 +0300

    feat: implement GetUserByID, DeleteUser, UpdateUser

--- internal/repository/mongo/mongo.go
+++ internal/repository/mongo/mongo.go
@@ -1,8 +1,8 @@
 package mongo
 
 import (
+	"errors"
 	"fmt"
-	"github.com/google/uuid"
 	"github.com/mellgit/someuser/internal/config"
 	"github.com/mellgit/someuser/internal/model"
 	"github.com/mellgit/someuser/internal/repository"
@@ -55,7 +55,7 @@ func (m MongoRepository) CreateUser(ctx context.Context, request model.CreateUse
 	if err != nil {
 		return nil, fmt.Errorf("failed to find inserted document: %w", err)
 	}
-	user.ID = resID.(bson.ObjectID).Hex()
+	//user.ID = resID.(bson.ObjectID).Hex()
 
 	return &user, nil
 }
@@ -75,17 +75,56 @@ func (m MongoRepository) GetAllUsers(ctx context.Context) (*[]model.SchemaSomeUs
 
 }
 
-func (m MongoRepository) GetUserByID(ctx context.Context, id uuid.UUID) (*model.SchemaSomeUser, error) {
-	//TODO implement me
-	panic("implement me")
+func (m MongoRepository) GetUserByID(ctx context.Context, id string) (*model.SchemaSomeUser, error) {
+
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id format: %w", err)
+	}
+
+	filter := bson.M{"_id": objectID}
+	var user model.SchemaSomeUser
+	if err := m.collection.FindOne(ctx, filter).Decode(&user); err != nil {
+		if errors.Is(mongo.ErrNoDocuments, err) {
+			return nil, fmt.Errorf("user not found: %w", err)
+		}
+		return nil, fmt.Errorf("failed to find user by id: %w", err)
+	}
+
+	return &user, nil
 }
 
-func (m MongoRepository) DeleteUser(ctx context.Context, id uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+func (m MongoRepository) DeleteUser(ctx context.Context, id string) error {
+
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return fmt.Errorf("invalid id format: %w", err)
+	}
+	filter := bson.M{"_id": objectID}
+	_, err = m.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+	return nil
 }
 
-func (m MongoRepository) UpdateUser(ctx context.Context, id uuid.UUID, request model.UpdateUserRequest) (*model.SchemaSomeUser, error) {
-	//TODO implement me
-	panic("implement me")
+func (m MongoRepository) UpdateUser(ctx context.Context, id string, request model.UpdateUserRequest) (*model.SchemaSomeUser, error) {
+
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id format: %w", err)
+	}
+	var user model.SchemaSomeUser
+	_, err = m.collection.UpdateOne(ctx, bson.M{"_id": objectID}, bson.M{"$set": request})
+	if err != nil {
+		return nil, fmt.Errorf("failed to update user: %w", err)
+	}
+	filter := bson.M{"_id": objectID}
+	err = m.collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find inserted document: %w", err)
+	}
+
+	return &user, nil
+
 }
