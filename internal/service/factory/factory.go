ccf2cc3ea44eb95f901dca44ccfe394a5b37111b
mellgit <daniilhorn8@gmail.com>
2025-04-17 14:48:35 +0300

    refactor: initialization service

--- internal/service/factory/factory.go
+++ internal/service/factory/factory.go
@@ -1,12 +1,18 @@
 package factory
 
 import (
+	"fmt"
 	"github.com/mellgit/someuser/internal/config"
 	"github.com/mellgit/someuser/internal/repository"
 	"github.com/mellgit/someuser/internal/service"
 	"github.com/mellgit/someuser/internal/service/someuser"
 )
 
-func NewService(cfg *config.Config, repo repository.Repository) service.Service {
-	return someuser.NewSomeUserService(cfg, repo)
+func NewService(cfg *config.Config, repo repository.Repository) (service.Service, error) {
+	switch cfg.Service {
+	case "someuser":
+		return someuser.NewSomeUserService(cfg, repo), nil
+	default:
+		return nil, fmt.Errorf("unknown service %v", cfg.Service)
+	}
 }
