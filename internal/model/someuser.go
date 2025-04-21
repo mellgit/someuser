5325053a8f2b934c1ff421177e8c0a9b32a5f1c3
mellgit <daniilhorn8@gmail.com>
2025-04-21 14:08:17 +0300

    feat: add validation in handler http and model

--- internal/model/someuser.go
+++ internal/model/someuser.go
@@ -1,19 +1,19 @@
 package model
 
 type CreateUserRequest struct {
-	Username string `bson:"username" json:"username"`
-	Email    string `bson:"email" json:"email"`
-	Password string `bson:"password" json:"password"`
+	Username string `bson:"username" json:"username" validate:"required,min=3,max=50"`
+	Email    string `bson:"email" json:"email" validate:"required,email"`
+	Password string `bson:"password" json:"password" validate:"gte=6,lte=50"`
 }
 
 type UpdateUserRequest struct {
-	Username string `bson:"username" json:"username"`
-	Email    string `bson:"email" json:"email"`
-	Password string `bson:"password" json:"password"`
+	Username string `bson:"username" json:"username" validate:"required,min=3,max=50"`
+	Email    string `bson:"email" json:"email" validate:"required,email"`
+	Password string `bson:"password" json:"password" validate:"gte=6,lte=50"`
 }
 
 type SchemaSomeUser struct {
-	ID       any    `db:"id" bson:"_id" json:"id,omitempty"`
+	ID       any    `db:"id" bson:"_id" json:"id,omitempty"` // validate:"-" - no validation
 	Username string `db:"username" bson:"username" json:"username"`
 	Email    string `db:"email" bson:"email" json:"email"`
 	Password string `db:"password" bson:"password" json:"password"`
